9e6413f9e8a683d466abcc392806b08735d5c434
Dawid Pytka <dawpyt169@gmail.com>
2022-11-11 18:19:37 +0100

    poprawki

--- App.js
+++ App.js
@@ -136,7 +136,125 @@ export default class App extends React.Component {
 
     const { count } = this.state;
 
-    const buttons = [
+    if (this.state.orientation === 'portrait') {
+          return (
+              <View style={styles.container}>
+                  <View style={styles.row}>
+                      <View style={styles.wynikV}>
+                          <Text style={styles.wynikT}>{this.state.count}</Text>
+                      </View>
+                  </View>
+                  {
+                      buttons.map(row => (
+                        <View style={styles.row}>
+
+                        {
+                          row.map((item, index) => (
+                            <TouchableOpacity disabled={item.disable} style={item.styl} onPress={item.onPress} key={index}>
+                              <Text>{item.title}</Text>
+                            </TouchableOpacity>
+                          ))
+                        }
+
+                        </View>
+                      ))
+                    }
+
+
+              </View>);
+
+
+
+    }else {
+        return (
+              <View style={styles.container}>
+                  <View style={styles.row}>
+                      <View style={styles.wynikV}>
+                          <Text style={styles.wynikT}>{this.state.count}</Text>
+                      </View>
+                  </View>
+                  {
+                      buttons2.map(row => (
+                        <View style={styles.row}>
+
+                        {
+                          row.map((item, index) => (
+                            <TouchableOpacity disabled={item.disable} style={item.styl} onPress={item.onPress} key={index}>
+                              <Text>{item.title}</Text>
+                            </TouchableOpacity>
+                          ))
+                        }
+
+                        </View>
+                      ))
+                    }
+
+
+              </View>);
+    }
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "flex-end",
+  },
+  button: {
+    flex: 1,
+    backgroundColor: "#DDDDDD",
+    padding: 22,
+    justifyContent: "center",
+    alignItems: "center",
+      marginLeft: 1,
+      marginRight: 1,
+      marginBottom: 1,
+      borderRadius: 1
+    },
+  buttonB: {
+    flex: 1,
+    backgroundColor: "blue",
+    padding: 22,
+    justifyContent: "center",
+    alignItems: "center",
+      marginLeft: 1,
+      marginRight: 1,
+      marginBottom: 1,
+      borderRadius: 1
+    },
+
+  row: {
+    maxwidth: '100%',
+    flexDirection: 'row',
+    backgroundColor: 'grey'
+  },
+  wynikV: {
+    padding: 10,
+    flexDirection: 'row',
+    flex: 1
+  },
+  wynikT: {
+    padding: 10,
+    color: 'white',
+    marginLeft: 'auto'
+  },
+  HwynikV: {
+    padding: 10,
+    flexDirection: 'row',
+    flex: 1
+  },
+  HwynikT: {
+    padding: 10,
+    color: 'white',
+    marginLeft: 'auto'
+  },
+  textB: {
+    color: 'white',
+    fontWeight: 'bold',
+  }
+});
+
+const buttons = [
                 [
                 {
                     styl: styles.button,
@@ -304,98 +422,250 @@ export default class App extends React.Component {
 
                 }]
             ]
+const buttons2 = [
+                [
+                {
+                    styl: styles.button,
+                    title: '√',
+                    disable: false,
+                    onPress: () => this.onScPress("sqrt"),
+                    color: 'black'
 
+                },
+                {
+                    styl: styles.button,
+                    title: 'x!',
+                    disable: false,
+                    onPress: () => this.onScPress("!"),
+                    color: 'black'
 
+                },
+                {
+                    styl: styles.button,
+                    title: 'AC',
+                    disable: false,
+                    onPress: () => this.onACPress(),
+                    color: 'black'
 
+                },
+                {
+                    styl: styles.button,
+                    title: '+/-',
+                    disable: false,
+                    onPress: () => this.onScPress("+/-"),
+                    color: 'black'
 
-          return (
-              <View style={styles.container}>
-                  <View style={styles.row}>
-                      <View style={styles.wynikV}>
-                          <Text style={styles.wynikT}>{this.state.count}</Text>
-                      </View>
-                  </View>
-                  {
-                      buttons.map(row => (
-                        <View style={styles.row}>
+                },
+                {
+                    styl: styles.button,
+                    title: '%',
+                    disable: false,
+                    onPress: () => this.onPress("%"),
+                    color: 'black'
 
-                        {
-                          row.map((item, index) => (
-                            <TouchableOpacity disabled={item.disable} style={item.styl} onPress={item.onPress} key={index}>
-                              <Text>{item.title}</Text>
-                            </TouchableOpacity>
-                          ))
-                        }
+                },
+                {
+                    styl: styles.buttonB,
+                    title: '/',
+                    disable: false,
+                    onPress: () => this.onPress("/"),
+                    color: 'white'
 
-                        </View>
-                      ))
-                    }
+                }],
+                [
+                                {
+                                    styl: styles.button,
+                                    title: 'e^x',
+                                    disable: false,
+                                    onPress: () => this.onScPress("e^x"),
+                                    color: 'black'
 
+                                },
+                                {
+                                    styl: styles.button,
+                                    title: '10^x',
+                                    disable: false,
+                                    onPress: () => this.onScPress("10^x"),
+                                    color: 'black'
 
-              </View>);
+                                },
+                                {
+                                    styl: styles.button,
+                                    title: '7',
+                                    disable: false,
+                                    onPress: () => this.onPress("7"),
+                                    color: 'black'
 
+                                },
+                                {
+                                    styl: styles.button,
+                                    title: '8',
+                                    disable: false,
+                                    onPress: () => this.onPress("8"),
+                                    color: 'black'
 
+                                },
+                                {
+                                    styl: styles.button,
+                                    title: '9',
+                                    disable: false,
+                                    onPress: () => this.onPress("9"),
+                                    color: 'black'
 
+                                },
+                                {
+                                    styl: styles.buttonB,
+                                    title: '*',
+                                    disable: false,
+                                    onPress: () => this.onPress("*"),
+                                    color: 'white'
 
-  }
-}
+                                }],
+                [
+                                {
+                                    styl: styles.button,
+                                    title: 'ln',
+                                    disable: false,
+                                    onPress: () => this.onScPress("ln"),
+                                    color: 'black'
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "flex-end",
-  },
-  button: {
-    flex: 1,
-    backgroundColor: "#DDDDDD",
-    padding: 22,
-    justifyContent: "center",
-    alignItems: "center",
-      marginLeft: 1,
-      marginRight: 1,
-      marginBottom: 1,
-      borderRadius: 1
-    },
-  buttonB: {
-    flex: 1,
-    backgroundColor: "blue",
-    padding: 22,
-    justifyContent: "center",
-    alignItems: "center",
-      marginLeft: 1,
-      marginRight: 1,
-      marginBottom: 1,
-      borderRadius: 1
-    },
+                                },
+                                {
+                                    styl: styles.button,
+                                    title: 'log10',
+                                    disable: false,
+                                    onPress: () => this.onScPress("log10"),
+                                    color: 'black'
 
-  row: {
-    maxwidth: '100%',
-    flexDirection: 'row',
-    backgroundColor: 'grey'
-  },
-  wynikV: {
-    padding: 10,
-    flexDirection: 'row',
-    flex: 1
-  },
-  wynikT: {
-    padding: 10,
-    color: 'white',
-    marginLeft: 'auto'
-  },
-  HwynikV: {
-    padding: 10,
-    flexDirection: 'row',
-    flex: 1
-  },
-  HwynikT: {
-    padding: 10,
-    color: 'white',
-    marginLeft: 'auto'
-  },
-  textB: {
-    color: 'white',
-    fontWeight: 'bold',
-  }
-});
+                                },
+                                {
+                                    styl: styles.button,
+                                    title: '4',
+                                    disable: false,
+                                    onPress: () => this.onPress("4"),
+                                    color: 'black'
 
+                                },
+                                {
+                                    styl: styles.button,
+                                    title: '5',
+                                    disable: false,
+                                    onPress: () => this.onPress("5"),
+                                    color: 'black'
+
+                                },
+                                {
+                                    styl: styles.button,
+                                    title: '6',
+                                    disable: false,
+                                    onPress: () => this.onPress("6"),
+                                    color: 'black'
+
+                                },
+                                {
+                                    styl: styles.buttonB,
+                                    title: '-',
+                                    disable: false,
+                                    onPress: () => this.onPress("-"),
+                                    color: 'white'
+
+                                }],
+                [
+                                {
+                                    styl: styles.button,
+                                    title: 'e',
+                                    disable: false,
+                                    onPress: () => this.onScPress("e"),
+                                    color: 'black'
+
+                                },
+                                {
+                                    styl: styles.button,
+                                    title: 'x^2',
+                                    disable: false,
+                                    onPress: () => this.onScPress("x^2"),
+                                    color: 'black'
+
+                                },
+                                {
+                                    styl: styles.button,
+                                    title: '1',
+                                    disable: false,
+                                    onPress: () => this.onPress("1"),
+                                    color: 'black'
+
+                                },
+                                {
+                                    styl: styles.button,
+                                    title: '2',
+                                    disable: false,
+                                    onPress: () => this.onPress("2"),
+                                    color: 'black'
+
+                                },
+                                {
+                                    styl: styles.button,
+                                    title: '3',
+                                    disable: false,
+                                    onPress: () => this.onPress("3"),
+                                    color: 'black'
+
+                                },
+                                {
+                                    styl: styles.buttonB,
+                                    title: '+',
+                                    disable: false,
+                                    onPress: () => this.onPress("+"),
+                                    color: 'white'
+
+                                }],
+                [
+                                {
+                                    styl: styles.button,
+                                    title: 'π',
+                                    disable: false,
+                                    onPress: () => this.onScPress("pi"),
+                                    color: 'black'
+
+                                },
+                                {
+                                    styl: styles.button,
+                                    title: 'x^3',
+                                    disable: false,
+                                    onPress: () => this.onScPress("x^3"),
+                                    color: 'black'
+
+                                },
+                                {
+                                    styl: styles.button,
+                                    title: '0',
+                                    disable: false,
+                                    onPress: () => this.onPress("0"),
+                                    color: 'black'
+
+                                },
+                                {
+                                    styl: styles.button,
+                                    title: '',
+                                    disable: true,
+                                    onPress: () => this.onScPress(),
+                                    color: 'black'
+
+                                },
+                                {
+                                    styl: styles.button,
+                                    title: ',',
+                                    disable: false,
+                                    onPress: () => this.onPress(","),
+                                    color: 'black'
+
+                                },
+                                {
+                                    styl: styles.buttonB,
+                                    title: '+',
+                                    disable: false,
+                                    onPress: () => this.onEQPress(),
+                                    color: 'white'
+
+                                }]
+            ]
